11b623659b8b17ec047811b032dbab84306a6478
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2025-02-16 09:56:39 -0300

    use test data from sample

--- src/fml/Main.java
+++ src/fml/Main.java
@@ -1,5 +1,8 @@
 package fml;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class Main {
@@ -7,103 +10,13 @@ public class Main {
     public static void main(String[] args) {
 
 //        String fmlData = "name:\"John Doe\";age:30;isStudent:true;grades:{9.5,8.0,7.5};";
-//        String fmlData = """
-//        name:"John Doe";
-//        age:30;
-//        isStudent:false;
-//        grades:{9.5,8.0,7.5};
-//        address:
-//            street:"road";
-//            number:666;
-//        ;
-//        """;
-//        String fmlData = """
-//            stringArr:{
-//              sub_str:
-//                v1:10;
-//                v2:"sub";
-//              ;,
-//              sub_str:
-//                v1:
-//                    v2:"sup";
-//                ;
-//              ;,
-//              sub_str:'g';
-//              v1:20;
-//              v2:"sut";
-//            };
-//        """;
-        String fmlData = """
-            # All data types of a FML file.
 
-            # Literals
-            string:"string \\"between\\" double quotes";
-            char:'c';
-            quote_in_char:'\\'';
-
-            # Numerics
-            int:10;
-            float:0.5;
-
-            # Booleans
-            boolTrue:true;
-            boolFalse:false;
-
-            # Vector
-            stringArr:{"yes", "no", "maybe"};
-            chargArr:{'a', 'b', 'c'};
-            intArr:{0, 1, 2, 10, -5};
-            floatArr:{0.9, 1.7, -0.2, 1.06, -5.618};
-            boolArr:{true, false, true};
-
-            # Matrix
-            multArr2:{
-              {0, 1, 2},
-              {0, 1, 2},
-              {0, 1, 2}
-            };
-
-            # Multidimensional array
-            multArr3:{
-              {
-                {0, 1, 2},
-                {0, 1, 2},
-                {0, 1, 2}
-              },
-              {
-                {0, 1, 2},
-                {0, 1, 2},
-                {0, 1, 2}
-              },
-              {
-                {0, 1, 2},
-                {0, 1, 2},
-                {0, 1, 2}
-              }
-            };
-
-            # Sub-structures
-            sub_str:
-              v1: 10;
-              v2: "sub";
-            ;
-
-            # Array of sub-structures
-            stringArr:{
-              sub_str:
-                v1:10;
-                v2:"sub";
-              ;,
-              sub_str:
-                v1:11;
-                v2:"sup";
-              ;,
-              sub_str:
-                v1:20;
-                v2:"sut";
-              ;
-            };
-        """;
+        String fmlData = "";
+        try {
+            fmlData = Files.readString(Paths.get("..\\samples\\all_data_structures.fml"));
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
 
         Map<String, Object> parsedData = FML.parse(fmlData);
         System.out.println("Parsed Data: " + parsedData);
